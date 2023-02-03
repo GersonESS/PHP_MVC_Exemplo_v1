a6d3e6149c56c38045af8ae205b1a58d27fade2e
Gerson Santos <gersones@outlook.com>
2023-02-03 19:05:04 -0300

    a

--- App/index.php
+++ App/index.php
@@ -5,7 +5,7 @@ include 'Controller/PessoaController.php';
 // Para saber mais sobre a função parse_url: https://www.php.net/manual/pt_BR/function.parse-url.php
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Para saber mais estrutura switch, leia: https://www.php.net/manual/pt_BR/control-structures.switch.php
+// Para saber  mais estrutura switch, leia: https://www.php.net/manual/pt_BR/control-structures.switch.php
 switch($url)
 {
     case '/':
